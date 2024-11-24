4c12cd85fba2c95e9d6677ce22009990ae3f62fb
Shaim <133188081+SMomin-7@users.noreply.github.com>
2024-11-23 21:55:55 -0700

    Initial idea

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/signup';
-
+import Navbar from './components/Navbar';
+import Dashboard from './pages/Dashboard';
 function App() {
   return (
     <Router>
+      <Navbar /> {/* Add Navbar here */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} /> {/* Add the Dashboard route */}
       </Routes>
     </Router>
   );
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'; // Import the CSS file
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">
+          BBET
+        </Link>
+        <ul className="navbar-menu">
+          <li>
+            <Link to="/" className="navbar-link">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/dashboard" className="navbar-link">
+              Dashboard
+            </Link>
+          </li>
+          <li>
+            <Link to="/login" className="navbar-link">
+              Login
+            </Link>
+          </li>
+          <li>
+            <Link to="/signup" className="navbar-link">
+              Signup
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
